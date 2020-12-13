040d674a9c9191071978ed894f76a8b6d8c5d876
MUYI <1120769355@qq.com>
2020-12-13 21:23:18 +0800

    else

--- d10_contextmanager.py
+++ d10_contextmanager.py
@@ -1,4 +1,12 @@
 # todo contextmanager
 """
+1、else 子句
+    a、和 if 搭配 “否则” 的意思
+    b、和 for、while、try/except 搭配
+        for..else.. 仅当 for 循环完毕 （没有 break，没有异常之类）执行 else 子句
+        while..else.. 仅当 while 因为条件为假退出 （没有 break，没有异常） 执行 else 子句
+        try..except..else.. 仅当 try 没有捕获到异常，执行 else 子句
 
+2、
 """
+
